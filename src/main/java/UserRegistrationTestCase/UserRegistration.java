54800af0e1c960c785963797014bac735ddc5131
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-04 15:58:15 +0530

    Added code for the first name with a test case

--- src/main/java/UserRegistrationTestCase/UserRegistration.java
+++ src/main/java/UserRegistrationTestCase/UserRegistration.java
@@ -1,10 +1,20 @@
 package UserRegistrationTestCase;
 
+import java.util.regex.*;
+
 public class UserRegistration {
 	
 	String welcomeMessage(String welcomeMessage)
-	{
-		
+	{		
 		return welcomeMessage;
 	}
+	
+	public static boolean firstNameValidation(String userInputFirstName)
+	{
+		Pattern firstNameRegExp=Pattern.compile("(^[A-Z]{1})[a-z]{2,}");
+		Matcher matchResult=firstNameRegExp.matcher(userInputFirstName);
+		boolean firstNameResult=matchResult.matches();
+		return firstNameResult;
+	}
+		
 }
--- src/test/java/UserRegistrationTestCase/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTestCase/UserRegistrationTest.java
@@ -5,11 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class UserRegistrationTest {
-
+	
+	@Test
 	public void welcomeMessageTest()
 	{
 		UserRegistration userRegistration=new UserRegistration();
 		String welcomeMessageResult=userRegistration.welcomeMessage("Welcome to user registration");
 		assertSame("Welcome to user registration",welcomeMessageResult);
 	}
+	
+	@Test
+	public void userRegistrationTest()
+	{
+		boolean firstNameResult=UserRegistration.firstNameValidation("Tejesh");		
+		assertEquals(true, firstNameResult);
+	}
+
+	
 }
